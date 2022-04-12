1ed0fa4aff4d1d2514ebd4a0f65d0f1a704f48ae
Marat_Galimov <72463817+Marat200@users.noreply.github.com>
2022-04-12 18:06:19 +0500

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,38 @@
+# просто эмуляция crud чтобы
+import random
+
+
+def get_page_context(page_name=''):
+    context = {}
+    navigation = [{'title': 'Home', 'href': '/', 'is_active': 'true'},
+                           {'title': 'About', 'href': '/about', 'is_active': 'false'},
+                           {'title': 'Contacts', 'href': '/contacts', 'is_active': 'false'}]
+    if page_name == 'index.html':
+        context = {
+
+            'info': 'This is my first home-task',
+
+        }
+
+    elif page_name == 'about.html':
+        context = {
+            'info': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been '
+                    'the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley '
+                    'of type and scrambled it to make a type specimen book. It has survived not only five centuries, '
+                    'but also the leap into electronic typesetting, remaining essentially unchanged. It was '
+                    'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, '
+                    'and more recently with desktop publishing software like Aldus PageMaker including versions of '
+                    'Lorem Ipsum. '
+        }
+
+    context['navigation'] = navigation
+    return context
+
+
+def is_user_covid_free():
+    # просто чтобы... ну там...
+    return random.choice([True, False])
+
+
+def get_covid_info():
+    return f'У тебя COVID! Оставайся дома и береги себя!'
